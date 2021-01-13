6ed69d9fa15e602901a77a63627e045872d240ea
AbhisheK Rathore <rathore.abhishek87@gmail.com>
2021-01-13 15:25:14 +0530

    completed linking

---
+++ app.js
@@ -0,0 +1,20 @@
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl)
+})
+
+
+
+function showToast(){
+    toastList[0].show()
+}
+
+// Enable Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+function goToProductDetails() {
+    window.location.href = "/product.html";
+}
